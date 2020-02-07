77057c87550945a9c999c3c035c8643e0732df63
Marek Pękala <marekbf3@gmail.com>
2020-02-08 00:30:47 +0100

    Update azure-pipelines.yml for Azure Pipelines

--- scripts/run_integration_tests_with_coverage.sh
+++ scripts/run_integration_tests_with_coverage.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-docker-compose -f ../IntegrationTests/docker-testenv/docker-compose.yml up --exit-code-from integrationtests 
+docker-compose -f ../IntegrationTests/docker-testenv/docker-compose.yml up --exit-code-from integrationtests
+docker-compose -f ../IntegrationTests/docker-testenv/docker-compose.yml down
--- scripts/run_unit_tests_with_coverage.sh
+++ scripts/run_unit_tests_with_coverage.sh
@@ -3,4 +3,4 @@
 set -e
 
 
-dotnet test ../UnitTests/UnitTests.csproj -p:CollectCoverage=true -p:MergeWith="../testReports/coverage.json" /maxcpucount:8 -p:CoverletOutput="../testReports/coverage" -p:CoverletOutputFormat="opencover"
+dotnet test ../UnitTests/UnitTests.csproj -p:CollectCoverage=true -p:MergeWith="../IntegrationTests/docker-testenv/testReports/coverage.json" /maxcpucount:8 -p:CoverletOutput="../UnitTests/testReports/coverage" -p:CoverletOutputFormat="opencover"
