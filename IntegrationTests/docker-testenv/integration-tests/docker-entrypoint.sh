c021a1ea4e5ade5dcf2d4cf40bee73b31b534f2d
Marek Pękala <marekbf3@gmail.com>
2020-02-12 01:08:54 +0100

    Update azure-pipelines.yml for Azure Pipelines

--- IntegrationTests/docker-testenv/integration-tests/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-export USE_SETTINGS="Y"
-wait-for-services.sh && dotnet test --no-build IntegrationTests.csproj /p:CollectCoverage=true /maxcpucount:8 /p:CoverletOutput="/reports/coverage"
